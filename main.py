3ef134c0b36d21509fab4afd0174ccaece9a9453
Ubuntu <ubuntu@ip-172-31-49-98.ec2.internal>
2015-07-06 01:41:45 +0000

    fix main to be functional

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 from flask import Flask, g, request
 
-from deepdream_test import *
+import deepdream_test
 
 app = Flask(__name__)
 
-@app.before_first_request
-def load_model():
-    g.net = deepdream_test.make_net('../caffe/models/bvlc_googlenet/')
+#@app.before_first_request
+#def load_model():
+#    g.net = deepdream_test.make_net('../caffe/models/bvlc_googlenet/')
 
 @app.route('/', methods=['GET', 'POST'])
 def make_dream():
+    net = deepdream_test.make_net('../caffe/models/bvlc_googlenet/')
     if request.method == 'POST' and len(request.files.keys()) > 0:
         key = request.files.keys()[0]
         filename = 'tmp/%s' % key
         with open(filename, 'wb') as file:
             file.write(request.files[key].read())
-        return deepdream_test.one_iter_deep(g.net, filename)
+        return deepdream_test.one_iter_deep(net, filename)
     #else if request.method == 'GET' and request.params['image_url']:
         # TODO: add the ability to dreamify a url image
     return 'No image found.'
 
+@app.route('/hello')
+def say_hello():
+    print "HELLO WORLD"
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=5000, debug=True)
