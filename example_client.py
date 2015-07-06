473de90a62d5a4414ed77bd6e5ed7e5d2afaca4c
Marcello Mathias Herreshoff <marcello.herreshoff@gmail.com>
2015-07-05 19:03:47 -0700

    Cleanup of example_client.py.

--- example_client.py
+++ example_client.py
@@ -2,7 +2,6 @@
 import argparse
 import os
 import requests
-import sys
 
 parser = argparse.ArgumentParser(
   description='Send a request to the Dreaming as a Service server.')
@@ -11,12 +10,12 @@ parser.add_argument('output_file_name', type=str)
 parser.add_argument('--server', type=str, default='52.7.11.64:5000')
 args = parser.parse_args()
 
-url = "http://%s/" % args.server
-print "Weirdifying %s -> %s" % (args.input_file_name, args.output_file_name)
-input_file = open(args.input_file_name, 'rb')
+print "Weirdifying %s -> %s..." % (args.input_file_name, args.output_file_name)
 
+input_file = open(args.input_file_name, 'rb')
 files={args.input_file_name: input_file}
 
+url = "http://%s/" % args.server
 response = requests.post(url, files=files)
 image_content = response.content
 
@@ -24,3 +23,4 @@ output_file = open(args.output_file_name, 'wb')
 output_file.write(image_content)
 output_file.close()
 
+print "Done."
